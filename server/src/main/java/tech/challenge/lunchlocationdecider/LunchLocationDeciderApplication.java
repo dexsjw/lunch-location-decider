b8b74d901df54d4adf3480ecb4141ba516dab690
dextersjw <dextersjw@gmail.com>
2024-03-30 04:57:54 +0800

    initial commit

---
+++ server/src/main/java/tech/challenge/lunchlocationdecider/LunchLocationDeciderApplication.java
@@ -0,0 +1,13 @@
+package tech.challenge.lunchlocationdecider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchLocationDeciderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchLocationDeciderApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/tech/challenge/lunchlocationdecider/LunchLocationDeciderApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.challenge.lunchlocationdecider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchLocationDeciderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
