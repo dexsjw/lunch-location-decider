fe33d55170e60af9de0ff63d9bee50d38e283784
dextersjw <dextersjw@gmail.com>
2024-04-01 20:39:24 +0800

    updated LunchSession with JPA annotations

--- server/src/main/java/tech/challenge/lunchlocationdecider/models/LunchSession.java
+++ server/src/main/java/tech/challenge/lunchlocationdecider/models/LunchSession.java
@@ -1,14 +1,41 @@
 package tech.challenge.lunchlocationdecider.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "lunch_session")
 public class LunchSession {
 
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "owner_code", nullable = false, unique = true)
     private String ownerCode;
+
+    @Column(name="room_code", nullable = false, unique = true)
     private String roomCode;
+
+    @Column(name="active_status", nullable = false)
     private boolean activeStatus;
+
+    @Column(name="restaurants")
     private String restaurants;
+
+    @Transient
+    private List<String> restaurantsList;
+
+    public String generateRestaurantsString(List<String> restaurantsList) {
+        return String.join(",", restaurantsList);
+    }
 
 }
