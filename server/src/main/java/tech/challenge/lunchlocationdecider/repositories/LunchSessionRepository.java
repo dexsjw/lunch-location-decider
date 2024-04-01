c91afbc7cbdccff6df0d8ee17d212fa5fd33ddcd
dextersjw <dextersjw@gmail.com>
2024-04-02 03:05:57 +0800

    updated LunchSessionRepository with customized methods

--- server/src/main/java/tech/challenge/lunchlocationdecider/repositories/LunchSessionRepository.java
+++ server/src/main/java/tech/challenge/lunchlocationdecider/repositories/LunchSessionRepository.java
@@ -1,9 +1,23 @@
 package tech.challenge.lunchlocationdecider.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tech.challenge.lunchlocationdecider.models.LunchSession;
 
 @Repository
 public interface LunchSessionRepository extends JpaRepository<LunchSession, Long> {
+
+    LunchSession findByRoomCode(String roomCode);
+
+    @Modifying(flushAutomatically = true)
+    @Query("UPDATE LunchSession ls SET ls.restaurants = :restaurants WHERE ls.roomCode = :roomCode")
+    void updateLunchSessionRestaurantsByRoomCode(@Param("restaurants") String restaurants, @Param("roomCode") String roomCode);
+
+    @Modifying(flushAutomatically = true)
+    @Query("UPDATE LunchSession ls SET ls.activeStatus = :activeStatus WHERE ls.roomCode = :roomCode")
+    void updateLunchSessionActiveStatusByRoomCode(@Param("activeStatus") boolean activeStatus, @Param("roomCode") String roomCode);
+
 }
