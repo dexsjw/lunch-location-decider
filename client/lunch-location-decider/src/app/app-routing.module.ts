e3e09c061dac8025583e4e56b1683fccf45a45dc
dextersjw <dextersjw@gmail.com>
2024-04-02 13:15:45 +0800

    updated app-routing.module.ts

--- client/lunch-location-decider/src/app/app-routing.module.ts
+++ client/lunch-location-decider/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { RoomComponent } from './room/room.component';
+import { SummaryComponent } from './summary/summary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full'},
+  { path: 'main', component: MainComponent },
+  { path: 'room', component: RoomComponent },
+  { path: 'summary', component: SummaryComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
