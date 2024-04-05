c57e53146ba9974c6418fb1e21e69a64171a04dd
Dexter <junweidexter.soh@dxc.com>
2024-04-05 21:08:53 +0800

    updated LunchSessionHelper with helper functions

--- server/lunch-location-decider/src/main/java/tech/challenge/lunchlocationdecider/helper/LunchSessionHelper.java
+++ server/lunch-location-decider/src/main/java/tech/challenge/lunchlocationdecider/helper/LunchSessionHelper.java
@@ -1,39 +1,55 @@
 package tech.challenge.lunchlocationdecider.helper;
 
+import java.util.Optional;
 import org.springframework.stereotype.Component;
-import tech.challenge.lunchlocationdecider.dto.LunchSessionDto;
+import tech.challenge.lunchlocationdecider.dto.LunchSessionRequestDto;
+import tech.challenge.lunchlocationdecider.dto.LunchSessionResponseDto;
 import tech.challenge.lunchlocationdecider.entity.LunchSessionEntity;
 
 @Component
 public class LunchSessionHelper {
 
-    public static LunchSessionDto toLunchSessionDto(LunchSessionEntity lunchSessionEntity, boolean hasOwnerCode) {
-        return new LunchSessionDto(hasOwnerCode, lunchSessionEntity.getOwnerCode(), lunchSessionEntity.getRoomCode(),
-                lunchSessionEntity.isActiveStatus(), lunchSessionEntity.getRestaurants(), "");
+    public static LunchSessionResponseDto toLunchSessionResponseDto(LunchSessionEntity lunchSessionEntity, boolean hasOwnerCode) {
+        return new LunchSessionResponseDto(hasOwnerCode, lunchSessionEntity.getRoomCode(),
+            lunchSessionEntity.isActiveStatus(), lunchSessionEntity.getRestaurants(), "");
     }
 
-    public static LunchSessionEntity toLunchSessionEntity(LunchSessionDto lunchSessionDto) {
-        return new LunchSessionEntity(0L, lunchSessionDto.getOwnerCode(), lunchSessionDto.getRoomCode(),
-                lunchSessionDto.isActiveStatus(), "");
+    public static LunchSessionEntity toLunchSessionEntity(LunchSessionRequestDto lunchSessionRequestDto) {
+        return new LunchSessionEntity(0L, lunchSessionRequestDto.getOwnerCode(), lunchSessionRequestDto.getRoomCode(),
+                lunchSessionRequestDto.isActiveStatus(), "");
+    }
+
+    public static LunchSessionEntity lunchSessionEntityNullCheck(LunchSessionEntity lunchSessionEntity) {
+        lunchSessionEntity.setOwnerCode(Optional.ofNullable(lunchSessionEntity.getOwnerCode()).orElseGet(() -> ""));
+        lunchSessionEntity.setRoomCode(Optional.ofNullable(lunchSessionEntity.getRoomCode()).orElseGet(() -> ""));
+        lunchSessionEntity.setRestaurants(Optional.ofNullable(lunchSessionEntity.getRestaurants()).orElseGet(() -> ""));
+        return lunchSessionEntity;
+    }
+
+    public static void lunchSessionDtoNullCheck(LunchSessionRequestDto lunchSessionRequestDto) {
+        lunchSessionRequestDto.setOwnerCode(Optional.ofNullable(lunchSessionRequestDto.getOwnerCode()).orElseGet(() -> ""));
+        lunchSessionRequestDto.setRoomCode(Optional.ofNullable(lunchSessionRequestDto.getRoomCode()).orElseGet(() -> ""));
+        lunchSessionRequestDto.setRestaurants(Optional.ofNullable(lunchSessionRequestDto.getRestaurants()).orElseGet(() -> ""));
+        lunchSessionRequestDto.setMessage(Optional.ofNullable(lunchSessionRequestDto.getMessage()).orElseGet(() -> ""));
     }
 
     public static String stringNullCheck(String str) {
         return str == null ? "" : str;
     }
 
-    public static LunchSessionEntity lunchSessionNullCheck(LunchSessionEntity lunchSessionEntity) {
-        lunchSessionEntity.setOwnerCode(stringNullCheck(lunchSessionEntity.getOwnerCode()));
-        lunchSessionEntity.setRoomCode(stringNullCheck(lunchSessionEntity.getRoomCode()));
-        lunchSessionEntity.setRestaurants(stringNullCheck(lunchSessionEntity.getRestaurants()));
-        return lunchSessionEntity;
-    }
+//    public static LunchSessionEntity lunchSessionNullCheck(LunchSessionEntity lunchSessionEntity) {
+//        lunchSessionEntity.setOwnerCode(stringNullCheck(lunchSessionEntity.getOwnerCode()));
+//        lunchSessionEntity.setRoomCode(stringNullCheck(lunchSessionEntity.getRoomCode()));
+//        lunchSessionEntity.setRestaurants(stringNullCheck(lunchSessionEntity.getRestaurants()));
+//        return lunchSessionEntity;
+//    }
 
-    public static LunchSessionDto lunchSessionDtoNullCheck(LunchSessionDto lunchSessionDto) {
-        lunchSessionDto.setOwnerCode(stringNullCheck(lunchSessionDto.getOwnerCode()));
-        lunchSessionDto.setRoomCode(stringNullCheck(lunchSessionDto.getRoomCode()));
-        lunchSessionDto.setRestaurants(stringNullCheck(lunchSessionDto.getRestaurants()));
-        lunchSessionDto.setMessage(stringNullCheck(lunchSessionDto.getMessage()));
-        return lunchSessionDto;
-    }
+//    public static LunchSessionDto lunchSessionDtoNullCheck(LunchSessionDto lunchSessionDto) {
+//        lunchSessionDto.setOwnerCode(stringNullCheck(lunchSessionDto.getOwnerCode()));
+//        lunchSessionDto.setRoomCode(stringNullCheck(lunchSessionDto.getRoomCode()));
+//        lunchSessionDto.setRestaurants(stringNullCheck(lunchSessionDto.getRestaurants()));
+//        lunchSessionDto.setMessage(stringNullCheck(lunchSessionDto.getMessage()));
+//        return lunchSessionDto;
+//    }
 
 }
