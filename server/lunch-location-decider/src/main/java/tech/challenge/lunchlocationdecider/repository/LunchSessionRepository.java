6b87e4a13f89e2250ba580530ca9cb70a89bc0a3
Dexter <junweidexter.soh@dxc.com>
2024-04-05 21:09:15 +0800

    commented out @Query functions

--- server/lunch-location-decider/src/main/java/tech/challenge/lunchlocationdecider/repository/LunchSessionRepository.java
+++ server/lunch-location-decider/src/main/java/tech/challenge/lunchlocationdecider/repository/LunchSessionRepository.java
@@ -14,14 +14,14 @@ public interface LunchSessionRepository extends JpaRepository<LunchSessionEntity
 
     Optional<LunchSessionEntity> findByRoomCode(String roomCode);
 
-    @Modifying(flushAutomatically = true)
-    @Query("UPDATE LunchSession ls SET ls.restaurants = :restaurants WHERE ls.roomCode = :roomCode")
-    void updateLunchSessionRestaurantsByRoomCode(@Param("restaurants") String restaurants, @Param("roomCode") String roomCode);
-
-    @Modifying(flushAutomatically = true)
-    @Query("UPDATE LunchSession ls SET ls.restaurants = :restaurants, ls.activeStatus = :activeStatus WHERE ls.roomCode = :roomCode")
-    void updateLunchSessionRestaurantsAndActiveStatusByRoomCode(@Param("restaurants") String restaurants,
-                                                                @Param("activeStatus") boolean activeStatus,
-                                                                @Param("roomCode") String roomCode);
+//    @Modifying(flushAutomatically = true)
+//    @Query("UPDATE LunchSession ls SET ls.restaurants = :restaurants WHERE ls.roomCode = :roomCode")
+//    void updateLunchSessionRestaurantsByRoomCode(@Param("restaurants") String restaurants, @Param("roomCode") String roomCode);
+//
+//    @Modifying(flushAutomatically = true)
+//    @Query("UPDATE LunchSession ls SET ls.restaurants = :restaurants, ls.activeStatus = :activeStatus WHERE ls.roomCode = :roomCode")
+//    void updateLunchSessionRestaurantsAndActiveStatusByRoomCode(@Param("restaurants") String restaurants,
+//                                                                @Param("activeStatus") boolean activeStatus,
+//                                                                @Param("roomCode") String roomCode);
 
 }
