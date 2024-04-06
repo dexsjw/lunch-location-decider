656b410eb1d17c5d602c125547fb0244eb9cd6c6
dextersjw <dextersjw@gmail.com>
2024-04-07 02:22:42 +0800

    updated main.component with new models

--- client/lunch-location-decider/src/app/main/main.component.ts
+++ client/lunch-location-decider/src/app/main/main.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { LunchSessionService } from '../service/lunch-session.service';
 import { Router } from '@angular/router';
-import { LunchSession } from '../models/lunch-session';
+import { LunchSessionRequest, LunchSessionResponse } from '../models/lunch-session';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogNoSessionComponent } from '../dialog-no-session/dialog-no-session.component';
 import { DataService } from '../service/data.service';
@@ -14,48 +14,50 @@ import { DataService } from '../service/data.service';
 })
 export class MainComponent{
 
-  lunchSessionForm: FormGroup = this.fb.group({
-    id: this.fb.control(''),
+  lunchSessionRequestForm: FormGroup = this.fb.group({
+    roomId: this.fb.control(''),
     ownerCode: this.fb.control(''),
-    roomCode: this.fb.control(''),
     activeStatus: this.fb.control(''),
-    restaurants: this.fb.control(''),
-    restaurantsList: this.fb.array([])
+    restaurant: this.fb.control('')
   });
 
   constructor(private lunchSessionSvc: LunchSessionService, private dataSvc: DataService, 
     private fb: FormBuilder, private router: Router, public dialog: MatDialog) {}
 
-  openDialog(lunchSession: LunchSession) {
-    const dialogRef = this.dialog.open(DialogNoSessionComponent, {data: lunchSession})
+  openDialog(lunchSessionResponse: LunchSessionResponse) {
+    const dialogRef = this.dialog.open(DialogNoSessionComponent, {data: lunchSessionResponse})
   }
 
   createNewLunchSession() {
-    const lunchSession: LunchSession = this.lunchSessionForm.value;
-    this.lunchSessionSvc.newLunchSession(lunchSession)
-      .then(lunchSession => {
-        // this.lunchSessionSvc.saveLunchSessionOwnerCodeLocally(lunchSession.ownerCode);
-        this.dataSvc.lunchSession = lunchSession;
-        this.dataSvc.saveLunchSessionOwnerCodeLocally(lunchSession.ownerCode);
+    this.lunchSessionSvc.newLunchSession()
+      .then(lunchSessionResponse => {
+        this.dataSvc.lunchSessionResponse = lunchSessionResponse;
+
+        localStorage.setItem(lunchSessionResponse.roomId, lunchSessionResponse.message ?? "");
         this.router.navigate(['/room']);
       })
-      .catch(err => console.error(err));
+      .catch(err => console.error(err.error));
   }
 
   findLunchSessionByRoomCode() {
-    const lunchSession: LunchSession = this.lunchSessionForm.value;
-    this.lunchSessionSvc.findLunchSession(lunchSession)
-      .then(lunchSession => {
-        this.dataSvc.lunchSession = lunchSession;
-        if (lunchSession.restaurants?.startsWith("<Error>:")) {
-          this.openDialog(lunchSession);
-        } else if (!lunchSession.activeStatus) {
+    const roomId: string = this.lunchSessionRequestForm.get("roomId")?.value ?? "";
+    const ownerCode: string = localStorage.getItem(roomId) ?? "";
+    this.lunchSessionRequestForm.get("ownerCode")?.setValue(ownerCode);
+    const lunchSessionRequest: LunchSessionRequest = this.lunchSessionRequestForm.value;
+
+    this.lunchSessionSvc.findLunchSession(lunchSessionRequest)
+      .then(lunchSessionResponse => {
+        this.dataSvc.lunchSessionResponse = lunchSessionResponse;
+
+        if (!lunchSessionResponse.activeStatus) {
           this.router.navigate(['/summary']);
         } else {
           this.router.navigate(['/room']);
         }
       })
-      .catch(err => console.error(err));
+      .catch(err => {
+        this.openDialog(err.error);
+      });
   }
 
 }
