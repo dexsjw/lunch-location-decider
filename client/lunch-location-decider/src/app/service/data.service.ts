bc608f6819d126452463e1c380a57f0356b457e8
dextersjw <dextersjw@gmail.com>
2024-04-07 02:22:01 +0800

    streamlined data.service functionality

--- client/lunch-location-decider/src/app/service/data.service.ts
+++ client/lunch-location-decider/src/app/service/data.service.ts
@@ -1,35 +1,19 @@
 import { Injectable } from '@angular/core';
-import { LunchSession } from '../models/lunch-session';
+import { LunchSessionResponse } from '../models/lunch-session';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  LUNCH_SESSION_OWNER_CODE: string = "LUNCH_SESSION_OWNER_CODE";
-
-  lunchSession: LunchSession = {
-    id: 0,
-    ownerCode: "",
-    roomCode: "",
+  lunchSessionResponse: LunchSessionResponse = {
+    roomId: "",
+    hasOwnerCode: false,
     activeStatus: false,
     restaurants: "",
-    restaurantsList: []
+    message: ""
   }
 
   constructor() { }
-
-  saveLunchSessionOwnerCodeLocally(ownerCode: string) {
-    localStorage.setItem(this.LUNCH_SESSION_OWNER_CODE, ownerCode);
-  }
-
-  hasLunchSessionOwnerCodeLocally(ownerCode: string): boolean {
-    const oc: string = localStorage.getItem(this.LUNCH_SESSION_OWNER_CODE) ?? "";
-    return ownerCode === oc;
-  }
-
-  clearLunchSessionOwnerCodeLocally() {
-    localStorage.clear();
-  }
 
 }
