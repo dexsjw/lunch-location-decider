f6d6e1760d9dd1599c290842e9c69857221f4d2d
dextersjw <dextersjw@gmail.com>
2024-04-07 23:30:15 +0800

    created LunchSessionServiceTest

---
+++ server/lunch-location-decider/src/test/java/tech/challenge/lunchlocationdecider/service/LunchSessionServiceTest.java
@@ -0,0 +1,27 @@
+package tech.challenge.lunchlocationdecider.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.challenge.lunchlocationdecider.dto.LunchSessionResponseDto;
+
+public class LunchSessionServiceTest {
+
+//    @Autowired
+//    LunchSessionService lunchSessionService;
+//
+//    @Test
+//    public void givenNoArgs_whenNewLunchSession_thenReturnLunchSesssionResponseDto() {
+//        LunchSessionResponseDto expectedLunchSessionResponseDto = new LunchSessionResponseDto(
+//                "E58ED763-928C-4155-BEE9-FDBAAADC15F3", true, true, "", "E58ED763"
+//        );
+//        LunchSessionResponseDto actualLunchSessionResponseDto = lunchSessionService.newLunchSession();
+//
+//        Assertions.assertNotEquals(expectedLunchSessionResponseDto.getRoomId(), actualLunchSessionResponseDto.getRoomId());
+//        Assertions.assertEquals(expectedLunchSessionResponseDto.isHasOwnerCode(), actualLunchSessionResponseDto.isHasOwnerCode());
+//        Assertions.assertEquals(expectedLunchSessionResponseDto.isActiveStatus(), actualLunchSessionResponseDto.isActiveStatus());
+//        Assertions.assertEquals(expectedLunchSessionResponseDto.getRestaurants(), actualLunchSessionResponseDto.getRestaurants());
+//        Assertions.assertNotEquals(expectedLunchSessionResponseDto.getMessage(), actualLunchSessionResponseDto.getMessage());
+//    }
+
+}
