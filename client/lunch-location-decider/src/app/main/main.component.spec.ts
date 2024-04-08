510446b3bea13116735a6fe9a6effc6f6a47fec4
dextersjw <dextersjw@gmail.com>
2024-04-08 08:23:34 +0800

    updated main.component test with some declarations

--- client/lunch-location-decider/src/app/main/main.component.spec.ts
+++ client/lunch-location-decider/src/app/main/main.component.spec.ts
@@ -1,14 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainComponent } from './main.component';
+import { LunchSessionResponse } from '../models/lunch-session';
+import { LunchSessionService } from '../service/lunch-session.service';
+import { DataService } from '../service/data.service';
 
 describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
+  class MockLunchSessionService {
+    mockResponse: LunchSessionResponse = {
+      roomId: '4df4b1b5-af80-475a-b6f7-f47e381678f3', 
+      hasOwnerCode: true, 
+      activeStatus: true, 
+      restaurants: '', 
+      message: '88cb0b20'
+    };
+  }
+
+  class MockDataService {
+    mockData: LunchSessionResponse = {
+      roomId: '4df4b1b5-af80-475a-b6f7-f47e381678f3', 
+      hasOwnerCode: true, 
+      activeStatus: true, 
+      restaurants: '', 
+      message: '88cb0b20'
+    };
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MainComponent]
+      declarations: [MainComponent],
+      providers: [MainComponent, 
+        { provide: LunchSessionService, useClass: MockLunchSessionService },
+        { provide: DataService, useClass: MockDataService }
+      ]
     })
     .compileComponents();
     
